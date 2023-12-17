c3e6d05d48847beef4cf879480b80f9ff76d9cd9
Tran Hoang Anh <119030498+HoangAnh3723@users.noreply.github.com>
2023-12-17 10:05:00 +0700

    lastest version

---
+++ Tranning/DatabaseConnection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Tranning
+{
+    public class DatabaseConnection
+    {
+        public static string getStrConnection()
+        {
+            string sTrConnection = @"Data Source=DESKTOP-6OVR3EK\TRANHOANGANH;Initial Catalog=Tranning;Integrated Security=True;TrustServerCertificate=True";
+            return sTrConnection;
+        }
+        public static SqlConnection GetSqlConnection() 
+        {
+            string StrConnection = DatabaseConnection.getStrConnection();
+            SqlConnection connection = new SqlConnection( StrConnection );
+            return connection;
+        }
+    }
+}
